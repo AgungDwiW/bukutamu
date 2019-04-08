5e26fbdcca88b36842f892223ff2dfa0dd009173
Unknown <magzksky@gmail.com>
2019-04-08 18:15:19 +0700

    Index V 0.1

---
+++ Index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Utama Buku Tamu</title>
+</head>
+<body>
+	<div id="Infotable">
+		<div id="Infofoto">
+			<img src="Person.jpg" height="225" width="180">
+		</div>
+		<div id="Infoform">
+			<h3>UID : <input type="text" name="UID" id="UID"></h3>
+			<h3>Type ID : <input type="text" name="TID" id="TID"></h3>
+			<h3>Nama : <input type="text" name="Nama" id="Nama"></h3>
+			<h3>Alamat : <input type="text" name="Alamat" id="Alamat"></h3>
+			<h3>No HP. : <input type="text" name="NoHP" id="NoHP"></h3>
+			<h3>Jenis Kelamin : <input type="text" name="Kelamin" id="Kelamin"></h3>
+			<h3>Institusi : <input type="text" name="Institusi" id="Institusi"></h3>
+			<h3>Suhu Badan : <input type="text" name="SuhuBadan" id="SuhuBadan"></h3>
+			<h3>Bertemu Dengan : <input type="text" name="Bertemu" id="Bertemu"></h3>
+			<h3>Keperluan : <input type="text" name="Keperluan" id="Keperluan"></h3>
+			<h3>Terdapat Luka terbuka : <input type="text" name="Luka" id="Luka"></h3>
+			<h3>Keperluan : <input type="text" name="Keperluan" id="Keperluan"></h3>
+			<h3>Sakit Terakhir : <input type="text" name="Sakit" id="Sakit"></h3>
+
+		</div>
+		<div id="infohistory">
+			<tr>
+    			<td>Tanggal Kunjungan</td>
+   				<td>Bertemu dengan</td>
+    			<td>Keperluan</td>
+  			</tr>
+		</div>
+
+	</div>
+
+</body>
+</html>
