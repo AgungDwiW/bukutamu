2106f92f5537517cf6e14abd6b928f881a0996e1
proyeksi <pif.zwei@gmail.com>
2019-06-11 10:33:57 +0700

    delete file

--- bukutamu/admin.py
+++ bukutamu/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Tamu
+from .models import Kedatangan
 
 admin.site.register(Tamu)
+admin.site.register(Kedatangan)
 # Register your models here.
--- pelaporan/models.py
+++ pelaporan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import bukutamu.models as bukutamu
+
 
 # Create your models here.
+
+class pelaporan (models.Model):
+	tamu = models.ForeignKey(bukutamu.Tamu, on_delete= models.CASCADE)
+	pelanggaran = models.CharField(max_length=(100))
+	tempat = models.CharField(max_length=(100))
+	tanggal = models.DateTimeField()
