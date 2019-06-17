9e38b76464c80bcad45232ddc7bb4ab68544c7af
proyeksi <pif.zwei@gmail.com>
2019-06-17 12:59:53 +0700

    autofocus login

--- bukutamu/admin.py
+++ bukutamu/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from .models import Tamu
 from .models import Kedatangan
-
+from pelaporan.models import Pelaporan
 
 # Register your models here.
 
 admin.site.register(Tamu)
 admin.site.register(Kedatangan)
+admin.site.register(Pelaporan)
 
